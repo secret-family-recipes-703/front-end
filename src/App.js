50172b0b12fcf5c3c6b20fd6352a6609e9ac07a8
David Temple <dstemple7@gmail.com>
2020-07-27 17:46:59 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import PrivateRoute from './util/PrivateRoute'
+// import PrivateRoute from './util/PrivateRoute'
 
 import './App.css';
 
