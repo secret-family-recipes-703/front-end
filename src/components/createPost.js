08f6d16d490fd5de36d2b229fc8bcc671f34bb40
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-07-27 15:46:04 -0700

    add recipe form somewhat finished

--- src/components/createPost.js
+++ src/components/createPost.js
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import axios from 'axios'
 import * as yup from 'yup'
+import newRecipe from '../validation/newRecipe'
+import Form from './createPostForm'
 
 //with title, source, ingredients, instructions, and category), and edit or delete it later
 const initialFormValues = {
     title: '',
     source: '',
-    ingredients: '',
-    instructions: '',
+    // ingredients: '',
+    // instructions: '',
     category: ''
   }
   const initialFormErrors = {
     title: '',
     source: '',
-    ingredients: '',
-    instructions: '',
+    // ingredients: '',
+    // instructions: '',
     category: '',
 
   }
   const initialDisabled = true
 
 
-const createPost = () => {
+const CreatePost = (props) => {
+// export default function CreatePost(props){
     const [recipe, setRecipe] = useState([])
     const [formValues, setFormValues] = useState(initialFormValues)
     const [formErrors, setFormErrors] = useState(initialFormErrors) 
     const [disabled, setDisabled] = useState(initialDisabled) 
 
-
-const postNewPizza = newRecipe => {
+const postNewRecipe = newRecipe => {
     axios.post('https://reqres.in/api/users', newRecipe)
       .then(res => {
         setRecipe([res.data, ...recipe])
@@ -41,10 +43,9 @@ const postNewPizza = newRecipe => {
       })
   }
 
-  const inputChange = evt => {
-    const { name, value } = evt.target
+  const inputChange = ( name, value ) => {
     yup
-      .reach(formSchema, name)
+      .reach(newRecipe, name)
       .validate(value)
       .then(valid => {
         setFormErrors({
@@ -64,87 +65,35 @@ const postNewPizza = newRecipe => {
     })
   }
 
-  const submit = evt => {
-    evt.preventDefault()
-    const newRecipe = {
-      title: formValues.title.trim(),
-      source: formValues.source.trim(),
-      ingreadients: formValues.ingredients.trim(),
-      instructions: formValues.instructions.trim(),
-      category: formValues.category,
-    }
-    postNewPizza(newRecipe)
-  }
+//   const submit = evt => {
+//     evt.preventDefault()
+//     const newRecipe = {
+//       title: formValues.title.trim(),
+//       source: formValues.source.trim(),
+//       ingreadients: formValues.ingredients.trim(),
+//       instructions: formValues.instructions.trim(),
+//       category: formValues.category,
+//     }
+//     postNewRecipe(newRecipe)
+//   }
   
   useEffect(() => {
-    formSchema.isValid(formValues).then(valid => {
+    newRecipe.isValid(formValues).then(valid => {
       setDisabled(!valid)
     })
   }, [formValues])
 
-//with title, source, ingredients, instructions, and category), and edit or delete it later
     return (
          <div>
-            <div className='errors'>
-                <div id='titleError'>{errors.title}</div>
-            </div>
-            <form>
-                <label htmlFor='title'>
-                    <input
-                    type='text'
-                    name='title'
-                    value={formValues.title}
-                    placeholder='Enter title of recipe'
-                    onChange={inputChange}
-                    ></input>
-                </label>
-                <label htmlFor='source'>
-                    <input
-                    type='text'
-                    name='source'
-                    value={formValues.source}
-                    placeholder='Enter source of recipe'
-                    onChange={inputChange}
-                    ></input>
-                </label>
-                <label htmlFor='ingredients'>
-                    <input
-                    type='text'
-                    name='ingredients'
-                    value={formValues.ingredients}
-                    placeholder='Enter ingredients for recipe'
-                    onChange={inputChange}
-                    ></input>
-                </label>
-                <label htmlFor='instructions'>
-                    <input
-                    type='text'
-                    name='instructions'
-                    value={formValues.instructions}
-                    placeholder='Enter instructions of recipe'
-                    onChange={inputChange}
-                    ></input>
-                </label>
-                <label htmlFor='category'>
-                    <select
-                    onChange={inputChange}
-                    value={formValues.category}
-                    name='category'
-                    >
-                        <option value='' >Food category</option>
-                        <option value='pizza' >Pizza</option>
-                        <option value='lame' >Not pizza</option>
-                    </select>
-                </label>
-
-                <button disabled={disabled} id='submitBtn'>Add Recipe</button>
-
-            </form>
-
-
+             <Form 
+             formErrors={formErrors}
+             formValues={formValues}
+             disabled={disabled}
+             inputChange={inputChange}
+             postNewRecipe={postNewRecipe}
+             />
          </div>
     )
 }
 
-
-export default createPost
+export default CreatePost
