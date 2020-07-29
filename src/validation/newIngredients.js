5e4c66fc0b1fc2ff0b1d3cd82dbc92749eedf514
David Temple <dstemple7@gmail.com>
2020-07-29 11:26:48 -0500

    intregrated kevin's pull request

--- src/validation/newIngredients.js
+++ src/validation/newIngredients.js
@@ -3,6 +3,7 @@ import * as yup from 'yup'
 const formSchema = yup.object().shape({
   ingredients: yup
     .string()
+    .min(3, 'ingredient should be atleast 3 characters')
     .required("Atleast one ingredient is required"),
 })
  
--- src/validation/newInstructions.js
+++ src/validation/newInstructions.js
@@ -3,6 +3,7 @@ import * as yup from 'yup'
 const formSchema = yup.object().shape({
   instructions: yup
     .string()
+    .min(3, 'Instructions must be atleast 3 characters')
     .required("Atleast one step is required"),
 })
  
