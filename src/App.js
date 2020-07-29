c4e7e02439d42e11c2d2d7143eac791d1c6c00fb
ianbautista <ianfbautista@gmail.com>
2020-07-29 04:52:19 -0700

    styled register and login pages. added fonts and images.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import PrivateRoute from './util/PrivateRoute'
 
-import './App.css';
+import "./App.css";
 
-import axiosWithAuth from './util/axiosWithAuth'
+import axiosWithAuth from "./util/axiosWithAuth";
 
-import {RecipeContext} from './contexts/Context'
+import { RecipeContext } from "./contexts/Context";
 
 //Components Import
-import Login from './components/Login'
-import Register from './components/Register'
-import Recipes from './components/Recipes'
-import NewRecipe from './components/New'
-
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Recipes from "./components/Recipes";
+import NewRecipe from "./components/New";
 
 function App() {
-  const [recipes, addRecipes] = useState([''])
+	const [recipes, addRecipes] = useState([""]);
 
-  return (
-    <Router>
-      <div className="App">
-        
-        <Route exact path="/" component={Register} className="RegisterComponent" />
-        {/* <PrivateRoute exact path="user" component={} />
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Register} className="RegisterComponent" />
+				{/* <PrivateRoute exact path="user" component={} />
         <PrivateRoute exact path="dashboard" component={} /> */}
-        <RecipeContext.Provider value={{recipes, addRecipes}}>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/recipes" component={Recipes} />
-          <Route exact path="/new" component={NewRecipe} />
-        </RecipeContext.Provider>
-      </div>
-    </Router>
-  );
+				<RecipeContext.Provider value={{ recipes, addRecipes }}>
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/recipes" component={Recipes} />
+					<Route exact path="/new" component={NewRecipe} />
+				</RecipeContext.Provider>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import * as yup from "yup";
+import "../App.css";
+import "../Form.css";
+import ChefWoman from "../images/woman-chef-kitchen-home.png";
+import Logo from "../images/SFRLogo.png";
+import Footer from "./Footer";
 
 const formSchema = yup.object().shape({
 	username: yup.string().min(3, "Username must be at least 3 characters").required(),
@@ -23,7 +28,7 @@ export default function Register() {
 	const [formValues, setFormValues] = useState(initialFormValues);
 	const [errors, setErrors] = useState(initialErrors);
 	const [buttonDisabled, setButtonDisabled] = useState(initialDisabled);
-	const {push} = useHistory()
+	const { push } = useHistory();
 
 	useEffect(() => {
 		formSchema.isValid(formValues).then((valid) => {
@@ -53,13 +58,13 @@ export default function Register() {
 		event.preventDefault();
 		axios
 			.post("https://secret-family-recipes-703.herokuapp.com/api/users/register", {
-				username: formValues.username, 
-				password: formValues.password
+				username: formValues.username,
+				password: formValues.password,
 			})
 			.then((response) => {
 				console.log("success");
 				setFormValues(initialFormValues);
-				push('/login')
+				push("/login");
 			})
 			.catch((error) => {
 				console.log(error.response);
@@ -78,45 +83,64 @@ export default function Register() {
 	};
 
 	return (
-		<div className="reister-form-contianer">
-			<h2> Register </h2>
-			<form onSubmit={formSubmit}>
-				<label htmlFor="username">
-					Username&nbsp;
-					<input
-						type="text"
-						name="username"
-						value={formValues.username}
-						onChange={inputChange}
-					/>
+		<>
+			<div className="register-header">
+				<div className="header-logo"></div>
+				<img className="header-chef-woman" src={ChefWoman} alt="chef women" />
+			</div>
+			<div className="register-form-contianer">
+				<h2> REGISTER </h2>
+				<form onSubmit={formSubmit}>
+					<div className="label-input-group">
+						<label className="reg-label" htmlFor="username">
+							Username&nbsp;&nbsp;&nbsp;
+						</label>
+						<input
+							type="text"
+							name="username"
+							value={formValues.username}
+							onChange={inputChange}
+						/>
+						<br />
+					</div>
 					{errors.username.length < 0 ? <p className="error">{errors.username}</p> : null}
-				</label>
-				<br />
-				<label htmlFor="password">
-					Password&nbsp;
-					<input
-						type="password"
-						name="password"
-						value={formValues.password}
-						onChange={inputChange}
-					/>
-				</label>
-				<br />
-				<label htmlFor="passwordConfirmation">
-					Confirm Password&nbsp;
-					<input type="password" name="passwordConfirmation" onChange={inputChange} />
+					<br />
+					<div className="label-input-group">
+						<label className="reg-label" htmlFor="password">
+							Password&nbsp;&nbsp;&nbsp;
+						</label>
+						<input
+							type="password"
+							name="password"
+							value={formValues.password}
+							onChange={inputChange}
+						/>
+					</div>
+					<br />
+					<div className="label-input-group">
+						<label className="reg-label" htmlFor="passwordConfirmation">
+							Confirm Password&nbsp;&nbsp;&nbsp;
+						</label>
+						<input type="password" name="passwordConfirmation" onChange={inputChange} /> <br />
+					</div>
 					{formValues.passwordConfirmation !== formValues.password ? (
 						<p className="error">{errors.passwordConfirmation}</p>
 					) : null}
-				</label>
-				<button disabled={buttonDisabled} name="submit">
-					Submit
-				</button>
-			</form>
-			<br />
-			<br />
-			<br />
-			<a href='/login'>Already Registered?</a>
-		</div>
+					<p className="form-terms-text">
+						By clicking on the register button you agree with Secret Family Recipes Terms &
+						Conditions, Fair Use, forever and ever so help you God.
+					</p>
+					<button disabled={buttonDisabled} name="submit">
+						REGISTER
+					</button>
+				</form>
+				<br />
+				<br />
+				<a className="already-text" href="/login">
+					Already Registered?
+				</a>
+			</div>
+			<Footer />
+		</>
 	);
 }
