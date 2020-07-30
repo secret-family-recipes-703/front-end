8a9b79bd17b97b167519c06f717309f29e25d90a
ianbautista <ianfbautista@gmail.com>
2020-07-29 17:18:39 -0700

    fixed styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "../App.css";
+// import "../App.css";
+import "../Footer.css";
 
 export default function Footer() {
 	return (
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useContext} from 'react'
-import axiosWithAuth from '../util/axiosWithAuth'
-import {useHistory, Link} from 'react-router-dom'
-import {RecipeContext} from '../contexts/Context'
-import NavBar from './navBar'
-import Recipe from './RecipeCard'
-import Styled from  'styled-components'
-import video from '../assets/strawberryVid.mp4'
-import SearchBar from './SearchBar'
-import '../styles/recipeList.css'
+import React, { useEffect, useContext } from "react";
+import axiosWithAuth from "../util/axiosWithAuth";
+import { useHistory, Link } from "react-router-dom";
+import { RecipeContext } from "../contexts/Context";
+import NavBar from "./navBar";
+import Recipe from "./recipeCard";
+import Styled from "styled-components";
+import video from "../assets/strawberryVid.mp4";
+import SearchBar from "./SearchBar";
+import "../styles/recipeList.css";
 
 const RecipesDiv = Styled.div`
 display: flex;
@@ -33,71 +33,49 @@ padding: 0 2rem;
             background-size: cover;
             /* background-position: center; */
         }
-`
+`;
 
 const Recipes = (props) => {
+	// const {searchValue} = useContext(RecipeContext)
+	const { recipes, addRecipes } = useContext(RecipeContext);
+	const { push } = useHistory();
 
-  // const {searchValue} = useContext(RecipeContext)
-  const {recipes, addRecipes} = useContext(RecipeContext)
-  const {push} = useHistory()
+	function routeToRecipe(ev, recipe) {
+		ev.preventDefault();
+		props.history.push(`/recipe/${recipe.id}`);
+	}
 
-  function routeToRecipe(ev, recipe) {
-    ev.preventDefault();
-    props.history.push(`/recipe/${recipe.id}`);
-  }
+	// 	return (
+	// 		<div className="recipes-container">
+	// 			<a href="/new">Add a New Recipe</a>
+	// 			<ul>
+	// 				{recipes.map((recipe) => (
+	// 					<ul className="recipe-cards">
+	// 						{recipe.name} // {recipe.category} // {recipe.source} <img src={recipe.imageURL} />
+	// 					</ul>
+	// 				))}
+	// 			</ul>
+	// 		</div>
+	// 	);
+	// };
 
 	return (
-		<div className="recipes-container">
-			<a href="/new">Add a New Recipe</a>
-			<ul>
-				{recipes.map((recipe) => (
-					<ul className="recipe-cards">
-						{recipe.name} // {recipe.category} // {recipe.source} <img src={recipe.imageURL} />
-					</ul>
-				))}
-			</ul>
+		<div>
+			<NavBar />
+			<RecipesDiv>
+				{recipes.map((recipe) => {
+					return (
+						<Link style={{ textDecoration: "none" }} to={`/recipe/${recipe.id}`}>
+							<Recipe recipe={recipe} />
+						</Link>
+					);
+				})}
+				<video id="videoBG" poster="../src/assets/strawberry.png" autoPlay muted loop>
+					<source src={video} type="video/mp4" />
+				</video>
+			</RecipesDiv>
 		</div>
 	);
 };
 
-return (
-      <div>
-        <NavBar/>
-        <RecipesDiv>
-          {
-          recipes.map(recipe => {
-            return <Link style={{textDecoration: 'none'}}to={`/recipe/${recipe.id}`}><Recipe recipe={recipe}/></Link>
-          })
-          }
-          <video id='videoBG' poster='../src/assets/strawberry.png' autoPlay muted loop>
-            <source src={video} type='video/mp4'/>
-          </video>
-        </RecipesDiv>
-
-      {/* <SearchBar />
-      <a href='/create'>Add a New Recipe</a>
-      <div className="recipeContainer">
-        {recipes.map(recipe => 
-          <div className="recipeCard" key={recipe.id}>
-          <Link to={`/recipe/${recipe.id}`}>
-            <img alt="recipe pic" src={recipe.imageURL}/>
-          </Link>
-            <br />
-            <button>View Ingredients + Instructions</button>
-            <br />
-            {recipe.name}
-            <br />
-            Category: {recipe.category}
-            <br />
-            by: {recipe.source}
-            <br />
-            <button>Edit Recipe</button>
-            <button>Delete Recipe</button>
-          </div>
-        )}
-        </div> */}
-      </div>
-    )
-}
-
-export default Recipes
+export default Recipes;
